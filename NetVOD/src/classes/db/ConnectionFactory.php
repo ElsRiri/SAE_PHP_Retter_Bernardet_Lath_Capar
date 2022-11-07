02139873444771e425aeb0091d7b0b41941bafdb
gretter6 <guillaume.retter5@etu.univ-lorraine.fr>
2022-11-07 10:39:08 +0100

    ajout antislash connectionFactory

--- NetVOD/src/classes/db/ConnectionFactory.php
+++ NetVOD/src/classes/db/ConnectionFactory.php
@@ -5,7 +5,7 @@ namespace NetVOD\db;
 class ConnectionFactory
 {
     public static array $tab;
-    public static ?PDO $db = null;
+    public static ?\PDO $db = null;
 
     public static function setConfig( string $file ){
         return self::$tab = parse_ini_file($file);
@@ -15,8 +15,8 @@ class ConnectionFactory
         $test = self::$tab['driver'].":host=".self::$tab['host']."; dbname=".self::$tab['database']."; charset=utf8";
         if(is_null(self::$db)) {
             try {
-                self::$db = new PDO($test, self::$tab['username'], self::$tab['password']);
-            } catch (Exception $e) {
+                self::$db = new \PDO($test, self::$tab['username'], self::$tab['password']);
+            } catch (\Exception $e) {
                 die('erreur ' . $e->getMessage());
             }
         }

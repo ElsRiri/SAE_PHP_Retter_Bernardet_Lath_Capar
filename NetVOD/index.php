e202156a1be9a5f42f0095a2b71e054450a25648
victor <victor.lath9@etu.univ-lorraine.fr>
2022-11-07 10:44:38 +0100

    try catch error db + correction nom classe

--- NetVOD/index.php
+++ NetVOD/index.php
@@ -1,12 +1,17 @@
 <?php
 require_once 'vendor/autoload.php';
-\NetVOD\db\ConnectionFactory::setConfig('db.config.ini');
-$db = \NetVOD\db\ConnectionFactory::makeConnection();
+try{
+    \NetVOD\db\ConnectionFactory::setConfig('db.config.ini');
+    $db = \NetVOD\db\ConnectionFactory::makeConnection();
+}catch (Error $ignored){
+    echo "problème avec la base de donnée.";
+}
+
 
 $action = null;
 $html = "";
 if (isset($_GET['action'])) {
     $action = $_GET['action'];
 }
-$disp = new \NetVod\dispatch\Dispatcher($action);
+$disp = new \NetVOD\dispatch\Dispatcher($action);
 $disp->run();

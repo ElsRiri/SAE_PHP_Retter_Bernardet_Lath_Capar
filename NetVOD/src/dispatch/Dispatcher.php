a055d855c43a4f584c7a091080066e04f2e5fffc
gretter6 <guillaume.retter5@etu.univ-lorraine.fr>
2022-11-07 14:14:46 +0100

    action DisplayCatalogueAction

--- NetVOD/src/dispatch/Dispatcher.php
+++ NetVOD/src/dispatch/Dispatcher.php
@@ -20,6 +20,11 @@ class Dispatcher
                 $str = $stmt->execute();
                 $this->renderPage($str);
                 break;
+            case 'DisplayCatalogueAction':
+                $stmt = new \NetVOD\action\DisplayCatalogueAction();
+                $str = $stmt->execute();
+                $this->renderPage($str);
+                break;
             default :
                 $str = "<H1>Netvod</H1>";
                 $this->renderPage($str);

23a6cca6117d320985a65e604f7e883e0e57155f
victor <victor.lath9@etu.univ-lorraine.fr>
2022-11-08 17:09:55 +0100

    DisplaySerieEnCoursAction.php

--- NetVOD/src/action/DisplaySerieEnCoursAction.php
+++ NetVOD/src/action/DisplaySerieEnCoursAction.php
@@ -13,7 +13,7 @@ class DisplaySerieEnCoursAction extends \NetVOD\action\Action
         $html = "";
         if ($_SERVER['REQUEST_METHOD'] == "GET") {
             if (isset($_SESSION['connexion'])){
-                $email_user = $_GET['connexion']['email'];
+                $email_user = $_SESSION['connexion']-> email;
                 $connexion = ConnectionFactory::makeConnection();
                 $stmt = $connexion-> prepare("select id from user where email = ?");
                 $stmt -> bindParam(1,$email_user);
@@ -21,10 +21,10 @@ class DisplaySerieEnCoursAction extends \NetVOD\action\Action
                 $resultatSet = $stmt->fetch(\PDO::FETCH_ASSOC);
                 $id_user = $resultatSet['id'];
 
-                $stmt = $connexion-> prepare("select id from serie where id = ?");
+                $stmt = $connexion-> prepare("select  * from ep_vision inner join serie on ep_vision.id_user = serie.id where id_user = ? group by serie.id");
                 $stmt -> bindParam(1,$id_user);
                 $stmt -> execute();
-                $html.= "Liste episode de: ".$_SESSION['connexion']['email'];
+                $html.= "Liste episode de: ".$_SESSION['connexion']->email;
                 while ($resultatSet = $stmt->fetch(\PDO::FETCH_ASSOC)){
                     $serie = new Serie($resultatSet['id'],$resultatSet['titre'],$resultatSet['descriptif'],$resultatSet['img'],$resultatSet['annee'],$resultatSet['date_ajout']);
                     $html.= $serie->render();

6a30df1fd198d21c9ce2e9dad17d9792b01e49ea
gretter6 <guillaume.retter5@etu.univ-lorraine.fr>
2022-11-08 15:38:34 +0100

    getPreference

--- NetVOD/src/User/User.php
+++ NetVOD/src/User/User.php
@@ -2,10 +2,13 @@
 
 namespace NetVOD\User;
 
+use NetVOD\db\ConnectionFactory;
+
 class User
 {
     private string $email;
     private string $passwd;
+
 
     public function __construct($email, $passwd)
     {
@@ -22,6 +25,20 @@ class User
     public function __set(string $at,mixed $val):void {
         if ( property_exists ($this, $at) ) $this->$at = $val;
         else throw new \Exception ("$at: invalid property");
+    }
+
+    public function getPreference():array{
+        $listVidePref = [];
+
+        $sql = "SELECT id_serie FROM preference";
+        $res = ConnectionFactory::$db->prepare($sql);
+        $res->execute();
+
+        while ($data = $res->fetch()){
+            array_push($listVidePref,$data[0]);
+        }
+
+        return $listVidePref;
     }
 
 }
